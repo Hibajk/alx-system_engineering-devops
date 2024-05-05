a3b510873356323792dab2d3b8ddcd26a78e0e7d
Hibajk <hibachenni22@gmail.com>
2024-05-05 06:13:42 -0700

    a

---
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -0,0 +1,7 @@
+#  setting up SSH configuration file
+
+
+exec {'config SSH client':
+  command => "echo -e 'IdentityFile ~/.ssh/school\nPasswordAuthentication no\n' >> /etc/ssh/ssh_config",
+  path    => '/bin:/usr/bin',
+}
